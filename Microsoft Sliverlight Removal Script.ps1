c8fb26a98274c82d9b42a7694e8ad07544f86ea9
Liam <31197356+zanthepuss@users.noreply.github.com>
2022-09-27 10:29:42 -0600

    Add files via upload

---
+++ Microsoft Sliverlight Removal Script.ps1
@@ -0,0 +1,5 @@
+﻿Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force 
+$result = gwmi win32_product -filter "Name LIKE 'Microsoft Silverlight'" | select IdentifyingNumber;
+[string] $a = $result.identifyingNumber;
+msiexec.exe /X $a /qn
+Remove-Item -Path "HKLM:\SOFTWARE\MMSOFT Design" -Recurse
